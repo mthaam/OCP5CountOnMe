d9f92f20bc214100be0714c760280e30e7e3d29b
mthaam <jsbrunet@protonmail.com>
2021-06-15 13:04:39 +0200

    added coverage to tests, and implemented tests to cover 100% of model.

--- CountOnMeTests/calculationModelTestCase.swift
+++ CountOnMeTests/calculationModelTestCase.swift
@@ -10,4 +10,66 @@ import XCTest
 
 class CalculationModelTestCase: XCTestCase {
 
+    var calculation: CalculationModel!
+
+    override func setUp() {
+        super.setUp()
+        calculation = CalculationModel()
+    }
+
+    func testGivenCalculTexPropertyHasAlreadyOneCharacter_WhenAddingANumber_ThenCalculTexPropertyCOntainsTheAddedNumber() {
+        calculation.calculText = "1"
+
+        calculation.numberButtonTapped(numberText: "2")
+
+        XCTAssert(calculation.calculText == "12")
+    }
+
+    func testGivenCalculTexPropertyHasAlreadyOneCharacter_WhenAddingMinusSign_ThenCalculTexPropertyCOntainsTheMinusSign() {
+        calculation.calculText = "1"
+
+        calculation.minusButtonTapped()
+
+        XCTAssert(calculation.calculText == "1 - ")
+    }
+
+    func testGivenCalculTexPropertyHasAlreadyOneCharacter_WhenAddingPlusSign_ThenCalculTexPropertyCOntainsThePlusSign() {
+        calculation.calculText = "1"
+
+        calculation.plusButtonTapped()
+
+        XCTAssert(calculation.calculText == "1 + ")
+    }
+
+    func testGivenCalculTexPropertyHasAlreadyOneCharacter_WhenAddingMultiplySign_ThenCalculTexPropertyCOntainsTheMultiplySign() {
+        calculation.calculText = "1"
+
+        calculation.multiplyButtonTapped()
+
+        XCTAssert(calculation.calculText == "1 x ")
+    }
+
+    func testGivenCalculTexPropertyHasAlreadyOneCharacter_WhenDivideSign_ThenCalculTexPropertyCOntainsTheDivideSign() {
+        calculation.calculText = "1"
+
+        calculation.divideButtonTapped()
+
+        XCTAssert(calculation.calculText == "1 / ")
+    }
+
+    func testGivenCalculTexPropertyHasAlreadyOneCharacter_WhenAddingEqualSign_ThenCalculTexPropertyCOntainsTheEqualSign() {
+        calculation.calculText = "1"
+
+        calculation.equalButtonTapped()
+
+        XCTAssert(calculation.calculText == "1 = ")
+    }
+
+    func testGivenCalculTexPropertyHasAlreadyOneCharacter_WhenClearing_ThenCalculTexPropertyCOntains0() {
+        calculation.calculText = "1"
+
+        calculation.clear()
+
+        XCTAssert(calculation.calculText == "0")
+    }
 }
